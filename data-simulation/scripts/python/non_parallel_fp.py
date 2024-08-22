685aded1806b27f6b08a5039903f3b57d121af50
Curtis Mitchell <70792951+curtisjm@users.noreply.github.com>
2024-08-22 15:32:36 -0700

    wrong file paths

--- data-simulation/scripts/python/non_parallel_fp.py
+++ data-simulation/scripts/python/non_parallel_fp.py
@@ -13,9 +13,9 @@ Notes:
 - Make each task larger to reduce overhead of starting a new one.
 '''
 
-BED_FILE = "~/Documents/bioinformatics/data-simulation/real-data/100_test.bed"
-OUT_DIR_DATA = "~/Documents/bioinformatics/data-simulation/simulated-data"
-OUT_DIR_REGIONS = "~/Documents/bioinformatics/data-simulation/simulated-data"
+BED_FILE = "../../real-data/100_test.bed"
+OUT_DIR_DATA = "../../simulated-data"
+OUT_DIR_REGIONS = "../../simulated-data"
 DEPTH = 25
 NUM_SAMPLES = 1
 #TODO: change this to standard deviation
